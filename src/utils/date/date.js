0abe2ce845c684afee92fcab1972a9b56cfc0f2e
rck <296046734@qq.com>
2022-04-07 12:05:24 +0800

    添加入职时间

--- src/utils/date/date.js
+++ src/utils/date/date.js
@@ -75,3 +75,16 @@ export function toTime(timestamp) {
   const D = date.getDate() + ' '
   return Y + M + D
 }
+
+
+export function datedifference(sDate1, sDate2) {    //sDate1和sDate2是2006-12-18格式
+  var dateSpan,
+      tempDate,
+      iDays;
+  sDate1 = Date.parse(sDate1);
+  sDate2 = Date.parse(sDate2);
+  dateSpan = sDate2 - sDate1;
+  dateSpan = Math.abs(dateSpan);
+  iDays = Math.floor(dateSpan / (24 * 3600 * 1000));
+  return iDays
+};
